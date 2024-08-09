1d7bb516899779f35ac49ff1a39a4ad03ca42942
weakbox <connormcleod2000@icloud.com>
2024-08-09 20:40:46 -0300

    Updated CSS and made dec/inc buttons clickable.

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,66 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Card } from 'react-bootstrap';
 
+const startTimer = () => {};
+const stopTimer = () => {};
+
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
+  const [timerIsRunning, setTimerIsRunning] = useState(false);
+
+  const decrementBreak = () => setBreakLength(b => b > 1 ? b - 1 : b);
+  const incrementBreak = () => setBreakLength(b => b < 60 ? b + 1 : b);
+ 
+  const decrementSession = () => setSessionLength(s => s > 1 ? s - 1 : s); 
+  const incrementSession = () => setSessionLength(s => s < 60 ? s + 1 : s);
+
+  const handleReset = () => {
+
+  };
+
+  const toggleTimer = () => {
+    if (timerIsRunning) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+    setTimerIsRunning(!timerIsRunning);
+  };
 
   return (
     <>
-      <h1>freeCodeCamp Pomodoro Clock</h1>
       <Card>
-        <Card.Body>
-          <Card.Title id="break-label">Break Length</Card.Title>
-          <Card.Subtitle className="text-muted">How long will the break be?</Card.Subtitle>
-          <Button id="break-decrement" className="btn-primary">-</Button>
-          <div id="break-length">{breakLength}</div>
-          <Button id="break-increment" className="btn-primary">+</Button>
+        <Card.Header id="break-label">Break Length</Card.Header>
+        <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+          <Card.Subtitle className="text-muted mb-2">How long will the break be?</Card.Subtitle>
+          <div className="d-flex flex-row justify-content-between align-items-center">
+            <Button id="break-decrement" className="btn-light" onClick={decrementBreak}>-</Button>
+            <h2 id="break-length" className="mx-3">{breakLength}</h2>
+            <Button id="break-increment" className="btn-light" onClick={incrementBreak}>+</Button>
+          </div>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Body>
-          <Card.Title id="session-label">Session Length</Card.Title>
-          <Card.Subtitle className="text-muted">How long will the work be?</Card.Subtitle>
-          <Button id="session-decrement" className="btn-primary">-</Button>
-          <div id="session-length">{sessionLength}</div>
-          <Button id="session-increment" className="btn-primary">+</Button>
+        <Card.Header id="session-label">Session Length</Card.Header>
+        <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+          <Card.Subtitle className="text-muted mb-2">How long will the work be?</Card.Subtitle>
+          <div className="d-flex flex-row justify-content-between align-items-center">
+            <Button id="session-decrement" className="btn-light" onClick={decrementSession}>-</Button>
+            <h2 id="session-length" className="mx-3">{sessionLength}</h2>
+            <Button id="session-increment" className="btn-light" onClick={incrementSession}>+</Button>
+          </div>
         </Card.Body>
       </Card>
       <Card>
-        <Card.Body>
-          <Card.Title id="timer-label">Session</Card.Title>
-          <div id="time-left">25:00</div>
-          <Button id="start-stop">Start/Stop</Button>
-          <Button id="reset" className="btn-danger">Reset</Button>
+        <Card.Header id="timer-label">Pomodoro Session</Card.Header>
+        <Card.Body className="d-flex flex-column justify-content-between align-items-center">
+          <Card.Subtitle className="text-muted mb-2">This many minutes until a break!</Card.Subtitle>
+          <h1 id="time-left" className="mb-2">{sessionLength}:00</h1>
+          <div className="d-flex flex-row justify-content-between align-items-center">
+            <Button id="start_stop" className="btn-primary me-1" onClick={toggleTimer}>Start/Stop</Button>
+            <Button id="reset" className="btn-danger ms-1" onClick={handleReset}>Reset</Button>
+          </div>
         </Card.Body>
       </Card>
     </>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FCC-Pomodoro-Clock/",
+  base: "/FCC-Pomodoro-Clock/", // Has to be the same name as GitHub repo!!!
 })
