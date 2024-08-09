4669a7cb4e3a2c5a4568c6b8f263b6dea3c20c3b
weakbox <connormcleod2000@icloud.com>
2024-08-09 16:10:22 -0300

    Added title.

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,29 @@ function App() {
 
   return (
     <>
+      <h1>freeCodeCamp Pomodoro Clock</h1>
       <Card>
         <Card.Body>
           <Card.Title id="break-label">Break Length</Card.Title>
           <Card.Subtitle className="text-muted">How long will the break be?</Card.Subtitle>
-          <Button id="break-decrement">-</Button>
+          <Button id="break-decrement" className="btn-primary">-</Button>
           <div id="break-length">{breakLength}</div>
-          <Button id="break-increment">+</Button>
+          <Button id="break-increment" className="btn-primary">+</Button>
         </Card.Body>
       </Card>
       <Card>
         <Card.Body>
           <Card.Title id="session-label">Session Length</Card.Title>
           <Card.Subtitle className="text-muted">How long will the work be?</Card.Subtitle>
-          <Button id="session-decrement">-</Button>
+          <Button id="session-decrement" className="btn-primary">-</Button>
           <div id="session-length">{sessionLength}</div>
-          <Button id="session-increment">+</Button>
+          <Button id="session-increment" className="btn-primary">+</Button>
         </Card.Body>
       </Card>
       <Card>
         <Card.Body>
           <Card.Title id="timer-label">Session</Card.Title>
-          <div id="time-left">mm:ss</div>
+          <div id="time-left">25:00</div>
           <Button id="start-stop">Start/Stop</Button>
           <Button id="reset" className="btn-danger">Reset</Button>
         </Card.Body>
